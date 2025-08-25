13c5436b2c9cdc912990fd1cc8d954b7b8803baf
Asma Saleem <asmasaleem@Asmas-MacBook-Pro.local>
2025-08-25 13:22:43 +0500

    Initial changes

---
+++ index1.ts
@@ -0,0 +1,52 @@
+interface Todo{
+    id : number;
+    title : string;
+    completed : boolean
+}
+
+let toDoArray : Todo[] = [];
+
+function addInTodo(title : string):void
+{
+    const newTodo : Todo = {
+        id : toDoArray.length +1,
+        title :title,
+        completed :false
+    }
+   toDoArray.push(newTodo);
+   console.log("Task is added successfully in Todo");
+}
+
+function listAll():void
+{
+    for(let task of toDoArray)
+    {
+      console.log(`ID : ${task.id}\nTITLE : ${task.title}\nCompleted : ${task.completed}`);
+      console.log();
+    }
+}
+function markAsDone(id : number):void
+{
+    let todo = toDoArray.find(t => t.id === id);
+    if (todo) {
+        todo.completed = true;
+        console.log(`Todo ${id} marked as complete.`);
+    } else {
+        console.log(`Todo with ID ${id} not found.`);
+    }
+    // for(let task of toDoArray)
+    // {
+    //    if(task.id === id)
+    //    {
+    //     task.completed = true;
+    //     console.log(`${task.title} is mark as done`);
+    //    }
+    //    else{
+    //     console.log(`${task.id} is not found`);
+    //    }
+    // }
+}
+addInTodo("Task1");
+addInTodo("Task2");
+listAll();
+markAsDone(123);
